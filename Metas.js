2bf84b2778e3243c0e4b5a5b0b585f533573872c
ketzakot <77943259+ketzakot@users.noreply.github.com>
2021-07-15 17:26:10 -0500

    Update Metas.js

--- Metas.js
+++ Metas.js
@@ -1,62 +1,9 @@
-const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const { WAconnetion, MessageType } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const prefix = '.'
 
-async function iniciar () { 
-        const client = new WAConnection()
-
-        client.logger.level = 'warn'
-
-//llamar al código QR
-        client.on('qr', () => {
-        })
-
-//crear un archivo Json para guardar información: ID del cliente, Token y Keys del cliente y del SERVER.
-        fs.existsSync('./package.json') && client.loadAuthInfo('./package.json')
-
-//Conectando o reconectando
-        client.on('connecting', () => {
-        console.log('Conectando')
-        })
-
-//La conexión fue en éxito👌🏻
-        client.on('open', () => {
-        console.log('Conectado exitosamente :D')
-        })
-        await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./package.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        
-
-client.on('chat-update', async (ketza) => 
-try {	  
-if (!ketza.hasNewMessage) return
-if (!ketza.messages) return
-if (ketza.key && ketza.key.remoteJid == 'status@broadcast') return
-
-ketza = ketza.messages.all()[0]
-if (!ketza.message) return
-global.blocked
-ketza.message = (Object.keys(ketza.message)[0] === 'ephemeralMessage') ? ketza.message.ephemeralMessage.message : ketza.message
-const from = ketza.key.remoteJid
-const type = Object.keys(ketza.message)[0]        
-const quoted = type == 'extendedTextMessage' && ketza.message.extendedTextMessage.contextInfo != null ? ketza.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
-const typeQuoted = Object.keys(quoted)[0]
-const content = JSON.stringify(ketza.message)
-const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
-const body = ketza.message.conversation || ketza.message[type].caption || ketza.message[type].text || ""
-chats = (type === 'conversation') ? ketza.message.conversation : (type === 'extendedTextMessage') ? ketza.message.extendedTextMessage.text : ''
-budy = (type === 'conversation' && ketza.message.conversation.startsWith(prefix)) ? ketza.message.conversation : (type == 'imageMessage') && ketza.message.imageMessage.caption.startsWith(prefix) ? ketza.message.imageMessage.caption : (type == 'videoMessage') && ketza.message.videoMessage.caption.startsWith(prefix) ? ketza.message.videoMessage.caption : (type == 'extendedTextMessage') && ketza.message.extendedTextMessage.text.startsWith(prefix) ? ketza.message.extendedTextMessage.text : ''
-
-if (prefix != "") {
-if (!body.startsWith(prefix)) {
-cmd = false
-comm = ""
-} else {const { WAConnection, MessageType } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const prefix = '.'
-
-async function iniciar () { 
-        const client = new WAConnection()
+async function iniciar () ² 
+        const { WAConnection()
 //Aquí el "client" lo pueden cambiar a su gusto. Pero si cambian, tendrán que cambiar todos los "client" por el cambio que hicieron.
         client.logger.level = 'warn'
 
