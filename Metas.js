d0ad9dada70dad9a29aba3ba629d8a741bfca42f
ketzakot <77943259+ketzakot@users.noreply.github.com>
2021-09-01 22:40:10 -0500

    Update Metas.js

--- Metas.js
+++ Metas.js
@@ -89,7 +89,7 @@ message: {
 }})
 break 	
 case 'audio':
-const audio = fs.readFileSync('./media/audio/audioM')
+const audio = fs.readFileSync('./media/audio/audioM.mp3')
 client.sendMessage(from, audio, MessageType.audio, {quoted: ketza, mimetype: 'audio/mp3', duration: -9999999, ptt: true})
 client.sendMessage(from, audio, MessageType.audio, {quoted: ketza, mimetype: 'audio/mp3', duration: -9999999})                
 break
