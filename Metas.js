b24f612ff6168219af0613199cbcc203ad4d669d
ketzakot <77943259+ketzakot@users.noreply.github.com>
2021-09-06 13:46:37 -0500

    Update Metas.js

--- Metas.js
+++ Metas.js
@@ -111,7 +111,7 @@ fromMe: false,
 participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
 },
 message: {
-"documentMessage": { "title": "Ketzakot", 'jpegThumbnail': fs.readFileSync('./media/logo/logo.jpg')}}
+"documentMessage": { "title": "Ketzakot", 'jpegThumbnail': fs.readFileSync('./media/logos/logo.jpg')}}
 }}) mimetype: 'audio/mp3', duration: -9999999, ptt: true})
                 
 break		
